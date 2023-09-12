a835ec78cec2c41c6886962bda444eece487906a
hashi7412 <egghead.kenzo@gmail.com>
2023-09-11 23:10:43 -0500

    Add: pakages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  eslint: {
+    dirs: ["pages", "utils"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
